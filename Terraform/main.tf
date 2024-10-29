15cf4f6f98143d1985c0d0486b713a423133ea9a
TziRed <robertescalante2014@gmail.com>
2024-10-29 02:08:53 +0000

    Learning about Terraform

---
+++ Terraform/main.tf
@@ -0,0 +1,27 @@
+resource "aws_instance" "vm1-terraform" { # "vm1-terraform" is the Logical ID
+  ami               = "ami-050cd642fd83388e4"
+  availability_zone = "us-east-2b"
+  instance_type     = "t2.micro"
+  key_name          = "vm_key"
+}
+
+resource "aws_iam_user" "user1" {
+  name = "RAE"
+}
+
+resource "aws_iam_user" "user2" {
+  name = "admins"
+}
+
+resource "aws_iam_group" "group1" {
+  name = "tfgroup"
+}
+
+resource "aws_iam_group_membership" "groupadd1" {
+  name = "tf-user_groupp_add"
+  users = [
+    aws_iam_user.user1.name,
+    aws_iam_user.user2.name
+  ]
+  group = aws_iam_group.group1.name
+}
---
+++ Terraform/providers.tf
@@ -0,0 +1,4 @@
+provider "aws" {
+  region  = "us-east-2" #Region of the AWS user
+  profile = "root"      #Name of the AWS user
+}
