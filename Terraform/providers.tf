b56f32291481e468a5459b3996b8181a312acda3
TziRed <robertescalante2014@gmail.com>
2024-11-02 19:02:04 +0000

    changes

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region              = "us-east-2" #Region of the AWS user
   profile             = "root"      #Name of the AWS user
-  shared_config_files = ["/home/ec2-user/.aws"]
-  shared_credentials_files = ["/home/ec2-user/.aws"]
+  shared_config_files = [ "/home/ec2-user/.aws/config" ]
+  shared_credentials_files = [ "/home/ec2-user/.aws/credentials" ]
 }
