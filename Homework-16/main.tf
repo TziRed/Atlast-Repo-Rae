6060db25dc276a85b09a835a73929e82052e498b
TziRed <robertescalante2014@gmail.com>
2024-11-15 02:35:40 +0000

    damn

--- Homework-16/main.tf
+++ Homework-16/main.tf
@@ -29,8 +29,8 @@ resource "aws_lb" "my_alb" {
   name               = "my-alb"
   internal           = false
   load_balancer_type = "application"
-  security_groups   = [aws_security_group.allow_http.id]
-  subnets            = ["subnet-089c5134affc617c3", "subnet-00278d7c2acf336a1"]  # Use your subnet IDs
+  security_groups    = [aws_security_group.allow_http.id]
+  subnets            = ["subnet-089c5134affc617c3", "subnet-00278d7c2acf336a1"] # Use your subnet IDs
 }
 
 # Target Group for the ALB
@@ -38,7 +38,7 @@ resource "aws_lb_target_group" "my_target_group" {
   name     = "my-target-group"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = "vpc-0cfcf8cf6704050e4"  # Use your VPC ID
+  vpc_id   = "vpc-0cfcf8cf6704050e4" # Use your VPC ID
 }
 
 # ALB Listener to forward traffic to the target group
@@ -55,18 +55,18 @@ resource "aws_lb_listener" "my_listener" {
 
 # Launch Template for EC2 Instances
 resource "aws_launch_template" "my_launch_template" {
-  name_prefix   = "my-template"
-  image_id      = "ami-0e593d2b811299b15"  # Use a valid AMI ID
-  instance_type = "t2.micro"
+  name_prefix          = "my-template"
+  image_id             = "ami-0e593d2b811299b15" # Use a valid AMI ID
+  instance_type        = "t2.micro"
   security_group_names = [aws_security_group.allow_http.name]
 }
 
 # Auto Scaling Group (ASG) to manage EC2 Instances
 resource "aws_autoscaling_group" "my_asg" {
-  desired_capacity     = 2
-  max_size             = 3
-  min_size             = 2
-  vpc_zone_identifier  = ["subnet-089c5134affc617c3", "subnet-00278d7c2acf336a1"]  # Use your subnet IDs
+  desired_capacity    = 2
+  max_size            = 3
+  min_size            = 2
+  vpc_zone_identifier = ["subnet-089c5134affc617c3", "subnet-00278d7c2acf336a1"] # Use your subnet IDs
 
   launch_template {
     id      = aws_launch_template.my_launch_template.id
