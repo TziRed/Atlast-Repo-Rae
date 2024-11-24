1ebb092bc76ebf011fe975e23378ff66b475c801
TziRed <robertescalante2014.gmail>
2024-11-24 17:31:34 +0000

    this shit better work

--- Homework-15/providers.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {
-  region                   = "us-east-2" #Region of the AWS user
-  profile                  = "root"      #Name of the AWS user
-  shared_config_files      = ["/home/ec2-user/.aws/config"]
-  shared_credentials_files = ["/home/ec2-user/.aws/credentials"]
-}
--- RaeCustomScripts/vars.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-## This cript is about variables.
-echo "This is a greting script"
-
-greeting="Bonjour"
-user=$(whoami)
-
-echo
-echo "Welcome $user, $greeting"
